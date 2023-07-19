6add2474a2d3c7c318c06d5519b1823ea4494a74
JOTALGS <jose,pgilsuarez@gmail.com>
2023-07-19 09:49:04 -0700

    $2

---
+++ 0-printf_source.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdarg.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+_printf(const char *format, ...)
+{
+	va_list args;
+	size_t i = 0;
+	int y = 0;
+	char op;
+	char *str;
+
+	va_start(args, format);
+	while(format[i])
+	{
+		if (format[i] == '%')
+		{
+			op = format[i + 1];
+			switch (op)
+			{
+				case 'c':
+					putchar(va_arg(args, int));
+					break;
+				case 's':
+					str = va_arg(args, char*);
+					while(str[y] != '\0')
+					{
+						putchar(str[y]);
+						y++;
+					}
+					break;
+			}
+		}
+		else if (format[i] != op)
+		{
+			putchar(format[i]);
+		}
+		i++;
+	}
+	return(0);
+}
